6fa3991e335343c91a465a11c21c0a7ab032f3f9
zyc <zz@gmail.com>
2023-08-07 21:33:23 +0800

    init

---
+++ iou.py
@@ -0,0 +1,20 @@
+"""
+@Project ：mAP
+@File    ：iou.py
+@Author  ：46069
+@Date    ：2023/8/7 21:20
+"""
+
+
+def compute_iou(true_boxes, predict_box):
+    """
+
+    :param true_boxes: 真实框 [N,4]
+    :param predict_box: 预测框 [1,4]
+    :return: 返回某个预测框和所有真实框的交并比 [N]
+    """
+
+
+
+
+if __name__ == '__main__':
